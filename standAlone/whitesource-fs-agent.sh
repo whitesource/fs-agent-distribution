07a4c2b487321cff8a30da214210c67bceb709ec
ChenLuigi <chen.luigi@whitesourcesoftware.com>
2018-01-31 14:44:41 +0200

    WSE-212 create whitesource-fs-agent.sh to run the File system agent by script file

---
+++ standAlone/whitesource-fs-agent.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+curl -LJO https://github.com/whitesource/fs-agent-distribution/raw/master/standAlone/whitesource-fs-agent-18.1.1.jar
+
+curl -LJO https://github.com/whitesource/fs-agent-distribution/raw/master/standAlone/whitesource-fs-agent.config
+
+java -jar whitesource-fs-agent-18.1.1.jar -apiKey $2 -project $4 -d ~/
