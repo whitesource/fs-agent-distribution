d0156cb48e84ecdf0648659771af9a4337e80e19
eRez-ws <35656848+eRez-ws@users.noreply.github.com>
2018-02-01 17:11:49 +0200

    updates to the shell code

--- standAlone/wss_agent.sh
+++ standAlone/wss_agent.sh
@@ -1,9 +1,7 @@
 #!/bin/bash 
-ls
-echo 1
+
 curl -LJO https://github.com/whitesource/fs-agent-distribution/raw/master/standAlone/whitesource-fs-agent.jar
-echo 2
+
 curl -LJO https://github.com/whitesource/fs-agent-distribution/raw/master/standAlone/whitesource-fs-agent.config
-echo 3
+
 java -jar whitesource-fs-agent.jar "$@"
-echo 4
